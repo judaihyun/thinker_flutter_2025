e8382d17356b8cef57a081ac08993e1c51d1165d
daihyun ju <judaihyun1@gmail.com>
2025-06-30 14:48:09 +0900

    d

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,53 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(scaffoldBackgroundColor: Colors.black),
-      home: const ScrollPage(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('홈', style: TextStyle(color: Colors.blue)),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              '$_count',
+              style: TextStyle(color: Colors.redAccent, fontSize: 70),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _count++;
+                });
+              },
+              child: const Text('+1'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _count++;
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
